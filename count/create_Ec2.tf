52e53548430207a81c9dff5b5f3b47c04657ca2f
Sai Vardhan <saivardhanguduru@gmail.com>
2025-10-28 00:43:48 +0530

    count

---
+++ count/create_Ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "expense" {
+  count = length(var.instances)
+
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = var.environment =="prod" ? "t2.medium" : "t3.micro"
+  vpc_security_group_ids=[aws_security_group.allow_tls.id]
+
+  tags= merge(
+      var.ec2_tags,
+      {
+        Name=var.instances[coun.index]
+      }
+
+
+    )
+  
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_1"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}

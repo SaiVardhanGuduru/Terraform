01a995c04b417a7b116ec0fd310c766e2b89de96
Sai Vardhan <saivardhanguduru@gmail.com>
2025-10-27 16:59:56 +0530

    listec2

--- list_ec2.tf
+++ list_ec2.tf
@@ -1,10 +1,13 @@
-list "aws_instance" "example" {
+data "aws_instances" "all_running" {
   provider = aws
 
-  config {
-    filter {
-      name   = "instance-state-name"
-      values = ["running"]
-    }
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
   }
+}
+
+# You can then get the list of instance IDs like this:
+output "running_instance_ids" {
+  value = data.aws_instances.all_running.ids
 }

b508dab2236235eb7f49111550df3f6b167f1ac3
Sai Vardhan <saivardhanguduru@gmail.com>
2025-10-27 21:16:27 +0530

    variables in terraform

--- variables/create_Ec2.tf
+++ variables/create_Ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
-  instance_type = var.ami_id
+  instance_type = var.instance_type
   vpc_security_group_ids=[aws_security_group.allow_tls.id]
 
   tags = {

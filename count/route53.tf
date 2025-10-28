b0716318090d9758fddc57ecf19f037c42379df5
Sai Vardhan <saivardhanguduru@gmail.com>
2025-10-28 21:55:45 +0530

    r53

--- count/route53.tf
+++ count/route53.tf
@@ -1,4 +1,5 @@
 resource "aws_route53_record" "expense" {
+  count = length(var.instances)
   provider = aws.personal
   zone_id = var.zone_id
   name    = "${var.instances[count.index]}.${var.domain_name}" #interpolation

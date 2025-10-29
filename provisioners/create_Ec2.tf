a2cf48bf019e8d816d98e7a2601cc514e3b9491a
Sai Vardhan <saivardhanguduru@gmail.com>
2025-10-30 00:11:50 +0530

    provisioners

--- provisioners/create_Ec2.tf
+++ provisioners/create_Ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  instance_type = "t2.medium"
   vpc_security_group_ids=[aws_security_group.allow_tls.id]
 
   tags = {

a57c02a91c83d6cae9918b4d92b8903f3d8ab951
Sai Vardhan <saivardhanguduru@gmail.com>
2025-10-30 17:30:33 +0530

    multi-env with tfvars

--- TFvars/route53.tf
+++ TFvars/route53.tf
@@ -1,20 +1,10 @@
-# /* resource "aws_route53_record" "expense" {
-#   count = length(var.instances)
-#   provider = aws.personal
-#   zone_id = var.zone_id
-#   name    = "${var.instances[count.index]}.${var.domain_name}" #interpolation
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_instance.expense[count.index].private_ip] #list type
-#   allow_overwrite = true
-# }
-
-# resource "aws_route53_record" "frontend" {
-#   provider = aws.personal
-#   zone_id = var.zone_id
-#   name    = "${var.domain_name}"
-#   type    = "A"
-#   ttl     = 1
-#   records = [aws_instance.expense[2].public_ip] #list type
-#   allow_overwrite = true
-# } */
+resource "aws_route53_record" "expense" {
+  count = length(var.instances)
+  provider = aws.personal
+  zone_id = var.zone_id
+  name    = var.instances[count.index] == "frontend" && var.environment == "prod" ? var.domain_name : "${var.instances[count.index]}-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [var.instances[count.index] == "frontend" && var.environment == "prod" ? aws_instance.expense[count.index].public_ip : aws_instance.expense[count.index].private_ip] #list type
+  allow_overwrite = true
+}

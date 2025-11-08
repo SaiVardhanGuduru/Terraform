c2f41856da7599a97df53b0018662ebbb0c7ef90
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-08 20:31:17 +0530

    vpc moduel test

--- vpcmodule_test/VPC.tf
+++ vpcmodule_test/VPC.tf
@@ -4,7 +4,6 @@ module "vpc" {
     cidr_block=var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
-    vpc_cidr = var.vpc_cidr
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
 

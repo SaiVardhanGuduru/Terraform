34fa033e622f529da1c15d09689c3bff62c9f310
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-08 20:26:50 +0530

    vpc module

--- VPCModule/outputs.tf
+++ VPCModule/outputs.tf
@@ -2,3 +2,6 @@
 #   value = local.default_vpc_id
 # }
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+ }
--- VPCModule/variables.tf
+++ VPCModule/variables.tf
@@ -1,37 +1,89 @@
-variable "project_name" {
-    default = "expense"
+variable "project_name"{
+
 }
 
 variable "environment"{
-    default = "dev"
+
 }
 
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+
 }
 
-variable "common_tags" {
-    default = {
-        Project = "expense"
-        Environment = "dev"
-        Terraform = "true"
-    }
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+# mandatory
+variable "common_tags"{
+    type = map
+    #default = {}
 }
 
 variable "vpc_tags" {
-    default = {
-        Purpose = "assignment"
-    }
+    default = {}
 }
 
-variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "igw_tags" {
+    default = {}
 }
 
-variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
-}
+# variable "public_subnet_cidrs" {
+#     type = list
+#     validation {
+#         condition     = length(var.public_subnet_cidrs) == 2
+#         error_message = "Please provide 2 valid public subnet CIDR"
+#     }
+# }
 
-variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
-}
+# variable "public_subnet_tags" {
+#     default = {}
+# }
+
+# variable "private_subnet_cidrs" {
+#     type = list
+#     validation {
+#         condition     = length(var.private_subnet_cidrs) == 2
+#         error_message = "Please provide 2 valid private subnet CIDR"
+#     }
+# }
+
+# variable "private_subnet_tags" {
+#     default = {}
+# }
+
+# variable "database_subnet_cidrs" {
+#     type = list
+#     validation {
+#         condition     = length(var.database_subnet_cidrs) == 2
+#         error_message = "Please provide 2 valid database subnet CIDR"
+#     }
+# }
+
+# variable "database_subnet_tags" {
+#     default = {}
+# }
+
+# variable "nat_gateway_tags" {
+#     default = {}
+# }
+
+# variable "public_route_table_tags" {
+#     default = {}
+# }
+
+# variable "private_route_table_tags" {
+#     default = {}
+# }
+
+# variable "database_route_table_tags" {
+#     default = {}
+# }
+
+# variable "is_peering_required" {
+#     default = false
+# }
+
+# variable "vpc_peering_tags" {
+#     default = {}
+# }

4e29559fe7a9bd938be2efa06f904f4d1edbb2b3
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-09 01:02:55 +0530

    vpc moduel test

--- vpcmodule_test/outputs.tf
+++ vpcmodule_test/outputs.tf
@@ -16,7 +16,7 @@ output "public_subnet_ids" {
 }
 
 output "private_subnet_ids" {
-  value = module.vpc.aws_subnet.private_subnet_ids
+  value = module.vpc.private_subnet_ids
 }
 
 output "database_subnet_ids" {

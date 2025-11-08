ec0436ecd52b1aa04ee5ed40aad8056744c47a3b
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-09 00:28:20 +0530

    vpc module

--- VPCModule/main.tf
+++ VPCModule/main.tf
@@ -67,6 +67,13 @@ resource "aws_subnet" "database" {
   )
 }
 
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+
+
+
+
 resource "aws_nat_gateway" "example" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id

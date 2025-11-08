107196c2f0a6c4b57de197e6a11c3d8603a5a6da
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-08 20:27:41 +0530

    vpc moduel test

--- vpcmodule_test/VPC.tf
+++ vpcmodule_test/VPC.tf
@@ -1,6 +1,13 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
     source = "git::https://github.com/SaiVardhanGuduru/Terraform.git//VPCModule?ref=main"
+    cidr_block=var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_cidr = var.vpc_cidr
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+
     
     
 }

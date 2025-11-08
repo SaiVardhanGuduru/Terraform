ff6b8e3b6ff8921c893b5f838c248d8319f92748
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-08 21:05:16 +0530

    vpc moduel test

--- vpcmodule_test/VPC.tf
+++ vpcmodule_test/VPC.tf
@@ -7,6 +7,10 @@ module "vpc" {
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
     vpc_name=var.vpc_name
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+
 
     
     

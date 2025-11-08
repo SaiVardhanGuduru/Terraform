40c89c985b0b958e1aebceee72918f66cbf5b554
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-08 20:51:54 +0530

    vpc module

--- VPCModule/locals.tf
+++ VPCModule/locals.tf
@@ -3,3 +3,7 @@
 #     default_vpc_id = data.aws_vpc.default.id
 #     default_vpc_cidr = data.aws_vpc.default.cidr_block
 # }
+
+locals {
+  resource_name = "${var.project_name}-${var.environment}"
+}
--- VPCModule/main.tf
+++ VPCModule/main.tf
@@ -4,3 +4,15 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
   tags = merge(var.common_tags,var.vpc_tags,var.vpc_name)
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags,
+    {
+      Name = local.resource_name
+    }
+  )
+}

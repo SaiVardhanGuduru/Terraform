6a9b5d6abff2cf991ac318f719f0880247d1dea1
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-26 16:43:02 +0530

    vpc, subnets for exense infra

---
+++ expense-infra/00-vpc/VPC.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/SaiVardhanGuduru/Terraform.git//VPCModule?ref=main"
+    vpc_cidr=var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    vpc_name=var.vpc_name
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+
+    
+    
+}

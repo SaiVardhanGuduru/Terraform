cb61c830f23c688ac9afdf27c5ea6fe9e53b0751
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-26 16:53:44 +0530

    securtiy groups for expense infra

---
+++ expense-infra/10-sg/sg.tf
@@ -0,0 +1,44 @@
+module "mysql_sg" {
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/SaiVardhanGuduru/Terraform.git//terraform-aws-securitygroup?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+
+
+
+module "bastion_sg" {
+    source = "git::https://github.com/SaiVardhanGuduru/Terraform.git//terraform-aws-securitygroup?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "bastion"
+    sg_description = "Created for bastion instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+# ports 22, 443, 1194, 943 --> VPN ports
+module "vpn_sg" {
+    source = "git::https://github.com/SaiVardhanGuduru/Terraform.git//terraform-aws-securitygroup?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "vpn"
+    sg_description = "Created for VPN instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "alb_ingress_sg" {
+    source = "git::https://github.com/SaiVardhanGuduru/Terraform.git//terraform-aws-securitygroup?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "app-alb"
+    sg_description = "Created for backend ALB in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}

24ad56d4064ede7f62c6c6b0c2c3cbc196ed381b
unknown <vardhan@TCS.WORKSPACESDEMO.COM>
2025-11-27 12:45:56 +0530

    expense-infra

--- expense-infra/20-bastion/bastion.tf
+++ expense-infra/20-bastion/bastion.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this" {
   ami                    = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
+  vpc_security_group_ids = [var.bastion_sg_id]
   instance_type          = "t3.micro"
   subnet_id   = local.public_subnet_id
 

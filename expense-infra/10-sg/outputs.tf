691cde6536ce5ce88093089b0edc8c42edb2fd61
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-28 20:40:21 +0530

    expense infra

--- expense-infra/10-sg/outputs.tf
+++ expense-infra/10-sg/outputs.tf
@@ -2,6 +2,6 @@ output "bastion_sg_id" {
   value = module.bastion_sg.sg_id
 }
 
-output "app_alb_sg" {
+output "app_alb_sg_id" {
   value = module.app_alb_sg.sg_id
 }
--- expense-infra/50-app-alb/locals.tf
+++ expense-infra/50-app-alb/locals.tf
@@ -1,5 +1,5 @@
 locals {
   # StringList to List
   private_subnet_ids = var.private_subnet_ids[0]
-  app_alb_sg_id = var.app_alb_sg.sg_id
+  app_alb_sg_id = var.app_alb_sg_id
 }

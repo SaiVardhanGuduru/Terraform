a45fb27223c4aced0579612887b2eacb2a31472b
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-28 20:27:18 +0530

    expense infra

--- expense-infra/main.tf
+++ expense-infra/main.tf
@@ -1,12 +1,21 @@
 module "vpc" {
   source  = "./00-vpc"
+  providers = {
+    aws = aws
+  }
 }
 module "sg"{
     source = "./10-sg"
+    providers = {
+    aws = aws
+  }
     vpc_id = module.vpc.vpc_id
 }
 module "bastion"{
   source = "./20-bastion"
+  providers = {
+    aws = aws
+  }
   public_subnet_ids = module.vpc.public_subnet_ids
   bastion_sg_id=module.sg.bastion_sg_id
   #app_alb_sg = module.sg.app_alb_sg
@@ -14,12 +23,14 @@ module "bastion"{
 
 module "app_alb"{
   source = "./50-app-alb"
+  providers = {
+    aws          = aws
+    aws.personal = aws.personal
+  }
+
   vpc_id = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
   bastion_sg_id=module.sg.bastion_sg_id
   app_alb_sg = module.sg.app_alb_sg
-   providers = {
-    aws          = aws
-    aws.personal = aws.personal
-  }
+   
 }
---
+++ expense-infra/variables.tf
@@ -0,0 +1,6 @@
+variable "personal_access_key" {
+  
+}
+variable "personal_secret_key" {
+  
+}

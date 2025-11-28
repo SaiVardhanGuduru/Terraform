f2db8f54755561bac37465ece3eec649e9d34935
Sai Vardhan <saivardhanguduru@gmail.com>
2025-11-28 20:50:32 +0530

    expense infra

--- expense-infra/50-app-alb/provider.tf
+++ expense-infra/50-app-alb/provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      configuration_aliases = [aws.personal]
     }
-    
   }
-  
-}
-provider "aws" {
-  alias = "personal"
 }

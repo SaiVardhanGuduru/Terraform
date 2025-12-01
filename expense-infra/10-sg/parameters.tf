9d9f097977c04992789c55c8e0f041fa195895d9
Sai Vardhan <saivardhanguduru@gmail.com>
2025-12-01 16:50:26 +0530

    expense infra

--- expense-infra/10-sg/parameters.tf
+++ expense-infra/10-sg/parameters.tf
@@ -9,10 +9,12 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
   type  = "String"
   value = module.bastion_sg.sg_id
+  overwrite   = true 
 }
 
 resource "aws_ssm_parameter" "app_alb_sg" {
   name  = "/${var.project_name}/${var.environment}/app_alb_sg"
   type  = "String"
   value = module.app_alb_sg.sg_id
+  overwrite   = true 
 }
--- expense-infra/provider.tf
+++ expense-infra/provider.tf
@@ -1,22 +1,13 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      #version = "5.84.0"
-    }
-  }
-
-
-
-
-
-  backend "s3" {
-    bucket = "sai-tf-remote-state-29102"
+  
+backend "s3" {
+    bucket = "sai-tf-remote-state-29103"
     key    = "terraform-ec2-module" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "terraform-state-locking"
   }
 }
+
 
 provider "aws" {
   # Configuration options
